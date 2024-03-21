8dcdb12286ede363c8e5d77a11be0787d98a3103
fikilifadly <fikilifadly@gmail.com>
2024-03-21 21:25:42 +0700

    feat: add skeleton routers, config, and move app into bin/www

--- server/app.js
+++ server/app.js
@@ -2,8 +2,12 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const router = require("./routers");
 const app = express();
 
-app.listen(3000, () => {
-	console.log("Server running on port 3000");
-});
+app.use(cors());
+app.use(express.json());
+
+app.use(router);
+
+module.exports = app;
