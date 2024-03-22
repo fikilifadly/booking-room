c46e98491d64945cddc6a5e96dc3f580b5443b79
fikilifadly <fikilifadly@gmail.com>
2024-03-22 23:18:37 +0700

    feat: add middleware for authentication and errohandler

---
+++ server/middlewares/ErrorHandler.js
@@ -0,0 +1,22 @@
+const errorHandler = (err, req, res, next) => {
+	console.log("Error Handler: ", err.name, err);
+
+	switch (err.name) {
+		case "SequelizeValidationError":
+			res.status(400).json({ message: err.errors[0].message });
+			break;
+		case "SequelizeUniqueConstraintError":
+			res.status(400).json({ message: err.errors[0].message });
+			break;
+		case "SequelizeDatabaseError":
+			res.status(400).json({ message: "Something Wrong On Database" });
+			break;
+		case `${err.name}`:
+			res.status(err.status).json({ message: err.name });
+		default:
+			res.status(500).json({ message: "Internal Server Error" });
+			break;
+	}
+};
+
+module.exports = errorHandler;
