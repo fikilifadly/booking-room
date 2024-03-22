33e8845b736dcb012ec42701cb97d2aff35c2e15
fikilifadly <fikilifadly@gmail.com>
2024-03-22 21:54:58 +0700

    feat: add model validation

---
+++ server/models/room.js
@@ -0,0 +1,47 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+	class Room extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+		}
+	}
+	Room.init(
+		{
+			roomName: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				validate: {
+					notNull: {
+						msg: "Room name is required",
+					},
+					notEmpty: {
+						msg: "Room name is required",
+					},
+				},
+			},
+			costPerHour: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+				validate: {
+					notNull: {
+						msg: "Cost per Hour is required",
+					},
+					notEmpty: {
+						msg: "Cost per Hour is required",
+					},
+				},
+			},
+		},
+		{
+			sequelize,
+			modelName: "Room",
+		}
+	);
+	return Room;
+};
---
+++ server/models/roomusage.js
@@ -0,0 +1,95 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+	class RoomUsage extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+		}
+	}
+	RoomUsage.init(
+		{
+			clientId: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+				validate: {
+					notNull: {
+						msg: "Client id is required",
+					},
+					notEmpty: {
+						msg: "Client id is required",
+					},
+				},
+			},
+			roomId: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+				validate: {
+					notNull: {
+						msg: "Room id is required",
+					},
+					notEmpty: {
+						msg: "Room id is required",
+					},
+				},
+			},
+			startTime: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				validate: {
+					notNull: {
+						msg: "Start time is required",
+					},
+					notEmpty: {
+						msg: "Start time is required",
+					},
+				},
+			},
+			endTime: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				validate: {
+					notNull: {
+						msg: "End time is required",
+					},
+					notEmpty: {
+						msg: "End time is required",
+					},
+				},
+			},
+			bookingDate: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				validate: {
+					notNull: {
+						msg: "Booking date is required",
+					},
+					notEmpty: {
+						msg: "Booking date is required",
+					},
+				},
+			},
+			quotaUsed: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+				validate: {
+					notNull: {
+						msg: "Quota used is required",
+					},
+					notEmpty: {
+						msg: "Quota used is required",
+					},
+				},
+			},
+		},
+		{
+			sequelize,
+			modelName: "RoomUsage",
+		}
+	);
+	return RoomUsage;
+};
